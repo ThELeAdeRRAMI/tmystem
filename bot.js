512ce95a09ef4910b85f5f94d82dac51b7be9cdc
ThELeAdeRRAMI <44674539+ThELeAdeRRAMI@users.noreply.github.com>
2018-11-14 11:54:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -809,4 +809,32 @@ client.on('message', message => {
          }
  })
  
+client.on('message',message =>{
+    var prefix = "$";
+    if(message.content.startsWith(prefix + 'top')) {
+  message.guild.fetchInvites().then(i =>{
+  var invites = [];
+   
+  i.forEach(inv =>{
+    var [invs,i]=[{},null];
+     
+    if(inv.maxUses){
+        invs[inv.code] =+ inv.uses+"/"+inv.maxUses;
+    }else{
+        invs[inv.code] =+ inv.uses;
+    }
+        invites.push(`invite: ${inv.url} inviter: ${inv.inviter} \`${invs[inv.code]}\`;`);
+   
+  });
+  var embed = new Discord.RichEmbed()
+  .setColor("#000000")
+  .setDescription(`${invites.join(`\n`)+'\n\n**By:** '+message.author}`)
+  .setThumbnail("https://cdn.discordapp.com/avatars/508646913579679744/ea7d223551c4a00df31361abd03a6eb5.png?size=2048")
+           message.channel.send({ embed: embed });
+   
+  });
+   
+    }
+  });
+
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
