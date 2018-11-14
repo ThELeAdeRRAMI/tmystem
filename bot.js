d70d6d0886e7fbd21a9a605b2e803a131fab61bc
ThELeAdeRRAMI <44674539+ThELeAdeRRAMI@users.noreply.github.com>
2018-11-14 09:40:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -691,5 +691,19 @@ client.on('message', message => {
       message.author.sendEmbed(Embed11)
     }
 });
+
+client.on('message', message => {
+    if(message.content == '$member') { // البريفكس و الامر
+    const embed = new Discord.RichEmbed()
+    .setDescription(`**Members infoً”‹
+:green_heart: online:   ${message.guild.members.filter(m=>m.presence.status == 'online').size}
+:heart:dnd:       ${message.guild.members.filter(m=>m.presence.status == 'dnd').size}
+:yellow_heart: idle:      ${message.guild.members.filter(m=>m.presence.status == 'idle').size}   
+:black_heart: offline:   ${message.guild.members.filter(m=>m.presence.status == 'offline').size} 
+:blue_heart:   all:  ${message.guild.memberCount}**`)         
+         message.channel.send({embed});
+
+    }
+  });
  
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
