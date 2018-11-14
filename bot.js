0f7cf20f3ff8d60a621258e87b683420c4cb1f8b
ThELeAdeRRAMI <44674539+ThELeAdeRRAMI@users.noreply.github.com>
2018-11-14 09:30:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -631,7 +631,7 @@ client.on('message', message => {
     if (message.guild) {
    let embed = new Discord.RichEmbed()
     let args = message.content.split(' ').slice(1).join(' ');
-if(message.content.split(' ')[0] == prefix + 'bc') { // الامر
+if(message.content.split(' ')[0] == prefix + 'tm') { // الامر
     if (!args[1]) {
 message.channel.send("**bc <message>**");
 return;
