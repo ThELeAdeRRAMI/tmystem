cd26ee2db6b3f5ff0924254d9684ac4c58e882b9
ThELeAdeRRAMI <44674539+ThELeAdeRRAMI@users.noreply.github.com>
2018-11-14 08:56:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -593,5 +593,17 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
 }
 });
 
+var prefix= "$"; // البريفكس
+client.on("message", message => {
+    if(message.content.startsWith(prefix + 'create')) { // الامر
+     let args = message.content.split(" ").slice(1);
+       var nam = args.join(' ');
+    
+      if(!message.member.hasPermission('ADMINISTRATOR')) return    message.channel.send('`ADMINISTRATOR` للأسف هذه الخاصية تحتاج الى ').then(msg => msg.delete(6000))
+      if (!nam) return message.channel.send(`<@${message.author.id}> يجب عليك ادخال اسم`).then(msg => msg.delete(10000))
+      message.guild.createChannel(nam, 'voice').then(c => setTimeout(() => c.delete(), 120000)) // كل 60 تساوي دقيقة عدل عليها الوقت لي تبيه 
+      message.channel.send(`:ballot_box_with_check: TemporarySound : \`${nam}\``).then(c => setTimeout(() => c.edit(`<@${message.author.id}> :stopwatch:  انتهى وقت الروم الصوتي`), 120000))  // 120000 دقيقتان
+    }
+    });
  
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
