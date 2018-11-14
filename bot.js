ea091509c64651ebb32cef9f9ac7c8e2a3912eaa
ThELeAdeRRAMI <44674539+ThELeAdeRRAMI@users.noreply.github.com>
2018-11-14 09:20:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -624,5 +624,38 @@ let embed24 = new Discord.RichEmbed()
 msg.channel.sendEmbed(embed24)
 }
  });
+
+client.on('message', message => {
+    var prefix = "$" // البريفكس
+    let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+
+if(command == "draw") { // الامر
+    var Canvas = require('canvas')
+  , Image = new Canvas.Image
+  , canvas = new Canvas(450, 170)
+  , ctx = canvas.getContext('2d');
+  ctx.font = '30px Impact';
+  let args = message.content.split(" ").slice(1);
+  
+Image.src = canvas.toBuffer();
+
+    console.log(Image);
+ctx.drawImage(Image, 0, 0, Image.width / 470, Image.height / 170);
+ctx.fillText(args.join("  "),110, 70);
+
+
+ctx.beginPath();
+ctx.lineTo(50, 102);
+ctx.stroke();
+
+message.channel.sendFile(canvas.toBuffer());
+}
+}).on('ready', () => {
+
+});
  
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
