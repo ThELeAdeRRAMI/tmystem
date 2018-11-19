863150e2beddc09e67d5d866ababf6bbe3d05f24
ThELeAdeRRAMI <44674539+ThELeAdeRRAMI@users.noreply.github.com>
2018-11-19 21:01:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1151,4 +1151,18 @@ client.on('guildMemberAdd', member => {
       channel.sendEmbed(embed);
     });
 
+client.on("guildMemberAdd", m => {
+    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 21) {
+        m.ban();
+    };
+    function parseDate(str) {
+        var mdy = str.split('/');
+        return new Date(mdy[2], mdy[0]-1, mdy[1]);
+    };
+    
+    function datediff(first, second) {
+        return Math.round((second-first)/(1000*60*60*24));
+    };
+});
+
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
